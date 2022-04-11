fc52d54b0165848ededf678671a87a1c643e7d71
minasmoves <minusmovi@gmail.com>
2022-04-11 07:11:49 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point 
  *
  * Return: Always 0 (Success)
- *
- int main (void)
+ */
+ int main(void)
  {
  printf("with proper grammar, but the outcome is a peice of art,\n");
  return(0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
 printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: 4 byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(b));
