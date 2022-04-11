8c70d69f8938dfcf18328dbf8f4fd45b56212278
minasmoves <minusmovi@gmail.com>
2022-04-11 03:59:46 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,17 +18,19 @@ n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
-printf("last digit of %d is %d %s\n", n, n % 10);
+printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 
 else if (n % 10 == 0)
 {
-print("and is 0\n", n, n % 10);
+print("last digit of %d is %d and is 0\n", n, n % 10);
 }
+
 else 
 {
-printf("and is less than 6 and not 0\n", n, n % 10);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
+
 
 return (0);
 }
