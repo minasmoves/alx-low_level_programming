f5888f2b05ded1b37f4224f3a0842a81a4221248
minasmoves <minusmovi@gmail.com>
2022-04-14 01:46:34 -0700

    0.isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - evaluate if letter is uppercase.
+ * @c: print int
+ * Return: always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
