78412b7be30ec4ead00db129e2ad555427df293e
minasmoves <minusmovi@gmail.com>
2022-04-20 00:28:01 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include "main.h"
 
 int _putchar(char c);
@@ -14,6 +11,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif /* MAIN_H */
-
