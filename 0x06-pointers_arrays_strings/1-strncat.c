7e8f63da3434fb049f9ba5e566a3087cc057aacc
minasmoves <minusmovi@gmail.com>
2022-04-20 04:15:25 -0700

    0x06-pointers_arrays_strings git commit -m 0x06-pointers_arrays_strings git commit -m 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two stringes
+ * @dest: a pointer to character
+ * @src: a pointer to character it will be changed
+ * @n: value
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+int i, j;
