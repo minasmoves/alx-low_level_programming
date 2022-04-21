f2cf9976533a4b42675e3606e4b930c06b871a81
minasmoves <minusmovi@gmail.com>
2022-04-20 23:28:52 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,6 +8,23 @@
  */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
-int i, j;
+int c, i;
+
+c = 0;
+
+
+while (dest[c])
+c++;
+
+/**
+ * src doesnot need to be null terminated
+ * if it is comtains n or more bytes
+ */
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[c + i] = src[i];
+
+dest[c + i] = '\0';
+
+return (dest);
+}
